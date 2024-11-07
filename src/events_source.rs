76e8a8e41df3254da7eee89c86a056e5556a3a6a
Rob <rob@espressosys.com>
2024-11-07 15:18:30 -0500

    remove async std

--- src/events_source.rs
+++ src/events_source.rs
@@ -1,10 +1,9 @@
 use std::{marker::PhantomData, sync::Arc};
 
 use async_broadcast::{broadcast, InactiveReceiver, Sender as BroadcastSender};
-use async_std::future;
 use async_trait::async_trait;
 use futures::{
-    future::BoxFuture,
+    future::{ready, BoxFuture},
     stream::{BoxStream, Stream, StreamExt},
 };
 use hotshot_types::{
@@ -137,7 +136,7 @@ impl<Types: NodeType> EventsSource<Types> for EventsStreamer<Types> {
 
         if let Some(filter) = filter {
             receiver
-                .filter(move |event| future::ready(filter.should_broadcast(&event.as_ref().event)))
+                .filter(move |event| ready(filter.should_broadcast(&event.as_ref().event)))
                 .boxed()
         } else {
             receiver.boxed()
